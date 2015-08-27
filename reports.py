c12d97d5ad2adabfffcb611882e0b86ecfdf0d8a
Niharika Kohli <niharikakohli29@gmail.com>
2015-08-27 18:54:58 +0530

    Cleaning up code

--- reports.py
+++ reports.py
@@ -4,6 +4,7 @@ import datetime
 from displayTable import *
 
 class Reports:
+
 	def forgotten_articles( self, site, db ):
 		cur = db.cursor()
 		query = "SELECT p.page_title, p.page_touched, p.page_namespace, p.page_is_redirect FROM page p WHERE page_is_redirect = 0 AND page_namespace = 0 ORDER BY page_touched LIMIT 5"
@@ -12,6 +13,6 @@ class Reports:
 		content = []
 		content.append( ['Title', 'Last touched'] )
 		for row in cur.fetchall() :
-			content.append( [row[0], datetime.datetime.strptime( row[1],'%Y%m%d%H%M%S')] )
+			content.append( [row[0], str( datetime.datetime.strptime( row[1],'%Y%m%d%H%M%S') ) ] )
 		text = generate_wikitext( content )
 		page.save( text, summary = 'bot test edit' )
