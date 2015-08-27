7e3ba8b6522ea6bc33baa402be2a5c8e2a9f4eb1
Niharika Kohli <niharikakohli29@gmail.com>
2015-08-27 13:56:59 +0530

    Testing 1

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ db = MySQLdb.connect( host = credentials['host'], user = credentials['user'], pa
 
 cur = db.cursor()
 
-cur.execute( "SELECT p.page_title, p.page_touched FROM page p ORDER BY p.page_touched DESC LIMIT 20" )
+cur.execute( "SELECT p.page_title, p.page_touched FROM page p ORDER BY p.page_touched DESC LIMIT 10" )
 
 site = mwclient.Site('test.wikipedia.org')
 site.login( testbot['user'], testbot['pass'] )
@@ -22,9 +22,10 @@ page = site.Pages['DBR test']
 
 #print 'Page default text was: ', text
 
-newtext = '{| class="wikitable" |- !Title !Last touched |- '
+newtext = '{| class="wikitable" \n |- \n !Title \n !Last touched \n'
 
 for row in cur.fetchall() :
-    newtext = newtext + '| [[' + row[0] + ']] | ' + datetime.datetime.fromtimestamp( int(row[1])/1000 ).strftime('%Y-%m-%d') + '<br>'
+    newtext = newtext + '|- \n | [[' + row[0] + ']] \n | ' 
+    newtext = newtext + datetime.datetime.fromtimestamp( int(row[1])/1000 ).strftime('%Y-%m-%d') + '\n'
 
-page.save(newtext, summary = 'bot test edit 2')
+page.save(newtext, summary = 'bot test edit 6')
