c23dba3d0b269fc83e1f9b884c1c88e6f9eefb81
Niharika Kohli <niharikakohli29@gmail.com>
2015-08-28 13:45:50 +0530

    Translations?

--- displayTable.py
+++ displayTable.py
@@ -1,21 +1,15 @@
-import json
-from pprint import pprint
+import i18n/*
 
 # Function to generate the wikitext for the report page
 # @param wiki - Language code for the wiki (eg: 'en', 'es', 'it')
 # @param content - Python 2D array (nested list) containing table entries
 def display_report( wiki, content ):
-	
-	with open('en.json') as data_file:    
-    	data = json.load( data_file )
-		pprint( data )
-
 	wikitext = '{| class="wikitable sortable" style="width:100%; margin:auto;" \n |- \n'
 	collen = len( content[0] )
 	rowlen = len( content )
 
 	for x in range( 0, collen ):
-		wikitext = wikitext + '! ' + data[ str( content[0][x] ) ] + '\n'
+		wikitext = wikitext + '! ' + en[ str( content[0][x] ) ] + '\n'
 
 	for x in range( 1, rowlen ):
 		wikitext = wikitext + '|- \n'
