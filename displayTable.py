3b8b309371f346b0e98ca81f8ac420b1e057a5b6
Niharika Kohli <niharikakohli29@gmail.com>
2015-09-08 21:46:38 +0530

    Bug fixes

--- displayTable.py
+++ displayTable.py
@@ -13,7 +13,6 @@ def display_report( wiki, content, desc ):
 	dict_obj = i18n.lang_dicts[ str( wiki + 'dict') ]
 
 	wikitext = dict_obj[ str( desc ) ] + ' -- [[User:Community Tech bot|Community Tech bot]] ([[User talk:Community Tech bot|talk]]) <onlyinclude>~~~~~</onlyinclude> \n'
-	wikitext = wikitext + '\n Data as of: ' + time.strftime('%H:%M, %d %B %Y (UTC)') + '\n'
 	wikitext = wikitext + '{| class="wikitable sortable" style="width:100%; margin:auto;" \n |- \n'
 	collen = len( content[0] )
 	rowlen = len( content )
