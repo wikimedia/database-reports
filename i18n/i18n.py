efb4c3b702a598dfe371b498c712c027372940a7
kaldari <kaldari@gmail.com>
2015-09-18 17:53:47 -0700

    Better report descriptions

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -1,47 +1,47 @@
 lang_dicts = {
 
 	'endict' : {
-	    "forgotten-articles-desc": "List of non-disambiguation, non-redirect oldest 1000 articles. The 'Last Touched' timestamp is updated whenever the corresponding page changes in a way requiring it to be re-rendered, invalidating caches. Aside from editing this, it includes permission changes, creation or deletion of linked pages, and alteration of contained templates.",
-	    "forgotten-articles-title": "Title",
-	    "forgotten-articles-last-edited": "Last touched",
-	    "forgotten-articles-editcount": "Number of edits",
+		"forgotten-articles-desc": "List of 1000 articles that have not been touched in the longest time (excluding redirects and disambiguation pages). Articles are ‘touched’ by any action that invalidates their cache. This includes editing, permission changes, creation or deletion of linked pages, and alteration of transcluded templates.",
+		"forgotten-articles-title": "Title",
+		"forgotten-articles-last-edited": "Last touched",
+		"forgotten-articles-editcount": "Number of edits",
 
-	    "pagecount-desc": "The number of pages in each [https://en.wikipedia.org/wiki/Wikipedia:Namespace namespace].",
-	    "pagecount-namespace": "Namespace ID",
-	    "pagecount-total": "Total pages",
-	    "pagecount-redirect": "Pages with redirects",
-	    "pagecount-non-redirect": "Pages without redirects",
-	    "pagecount-namespace-name": "Namespace",
+		"pagecount-desc": "The number of pages in each [[Wikipedia:Namespace|namespace]]",
+		"pagecount-namespace": "Namespace ID",
+		"pagecount-total": "Total pages",
+		"pagecount-redirect": "Pages with redirects",
+		"pagecount-non-redirect": "Pages without redirects",
+		"pagecount-namespace-name": "Namespace",
 
-	    "pagerevisions-namespace": "Namespace",
-	    "pagerevisions-title": "Article",
-	    "pagerevisions-revisions": "Revisions",
-	    "pagerevisions-desc": "Pages with the most revisions (limited to the first 1000 entries)",
+		"pagerevisions-desc": "Pages with the most revisions (limited to the first 1000 entries)",
+		"pagerevisions-namespace": "Namespace",
+		"pagerevisions-title": "Article",
+		"pagerevisions-revisions": "Revisions",
 
-	    "blankpages-desc": "Blank pages with a single author",
-	    "blankpages-title": "Page",
+		"blankpages-desc": "Blank pages with a single author",
+		"blankpages-title": "Page",
 
-	    "autopatrol-desc": "Below is a list of editors who are eligible for the autopatrol privilege and don't have it yet. Limited to 500 users with most articles created.",
-	    "autopatrol-username": "Username",
-	    "autopatrol-articles": "Number of articles created",
+		"autopatrol-desc": "List of editors who are eligible for the autopatrol privilege and don't have it yet. Limited to 500 users with most articles created.",
+		"autopatrol-username": "Username",
+		"autopatrol-articles": "Number of articles created",
 
-	    "tpbs-desc": "Ranked tally of the total size of a given talk page, including all its subpages (e.g. archival subpages for a user talk page, individual WP:RFAs, etc.), to provide statistics on very active discussion pages.",
-	    "tpbs-namespace": "Namespace",
-	    "tpbs-size": "Size (in MB)",
-	    "tpbs-page": "Page",
+		"tpbs-desc": "Talk pages ranked by total size, including all subpages (e.g. archival subpages, individual WP:RFAs, etc.), to provide statistics on very active discussion pages.",
+		"tpbs-namespace": "Namespace",
+		"tpbs-size": "Size (in MB)",
+		"tpbs-page": "Page",
 	},
 
 	'esdict' : {
 		"forgotten-articles-desc": "List of non-disambiguation, non-redirect oldest 1000 articles. - Spanish",
-	    "forgotten-articles-title": "Title - Spanish",
-	    "forgotten-articles-last-edited": "Last edited - Spanish",
-	    "forgotten-articles-editcount": "Number of edits - Spanish",
+		"forgotten-articles-title": "Title - Spanish",
+		"forgotten-articles-last-edited": "Last edited - Spanish",
+		"forgotten-articles-editcount": "Number of edits - Spanish",
 
-	    "pagecount-desc": "The number of pages in each namespace. - Spanish",
-	    "pagecount-namespace": "Namespace - Spanish",
-	    "pagecount-total": "Total pages - Spanish",
-	    "pagecount-redirect": "Pages with redirects - Spanish",
-	    "pagecount-non-redirect": "Pages without redirects - Spanish",
+		"pagecount-desc": "The number of pages in each namespace. - Spanish",
+		"pagecount-namespace": "Namespace - Spanish",
+		"pagecount-total": "Total pages - Spanish",
+		"pagecount-redirect": "Pages with redirects - Spanish",
+		"pagecount-non-redirect": "Pages without redirects - Spanish",
 	}
 
 }
