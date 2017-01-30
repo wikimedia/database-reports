3ce8706495b37ec1d4ae707f55854f4df3575785
Framawiki <framawiki@tools.wmflabs.org>
2017-01-30 20:13:53 +0100

    fr dict

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -305,7 +305,7 @@ lang_dicts = {
 		"most_edited_page_last_month-editcount": "Nombre d'éditions",
 
 		"most_watched-page-title": "Pages les plus suivies",
-		"most_watched-desc": "Liste des pages non-supprimées les plus suivies (limitée aux 1 000 premières entrées)",
+		"most_watched-desc": "Liste des pages non-supprimées les plus suivies (limitée aux 1 000 premières entrées).  Il est possible de trier une colonne en cliquant sur les deux flèches de son libellé.",
 		"most_watched-namespace": "[[Aide:Espace de noms|NS]]",
 		"most_watched-title": "Titre",
 		"most_watched-watchers": "Nombre",
