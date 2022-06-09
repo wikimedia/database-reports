dda312e568bbdc553f01b1731108488b72455043
Sammy <375162+theresnotime@users.noreply.github.com>
2022-06-09 19:39:56 +0100

    Set up GitHub action to run a tox suite (flake8, black, isort, yaml) (#39)
    * Set up GitHub action to run a tox suite (flake8, black, isort, yaml)
    * Test against Python versions 3.5 to 3.10
    * (okay maybe not 3.5)
    * oooh and dependabot, why not, treat yourself
    * *another one*

--- displayTable.py
+++ displayTable.py
@@ -1,29 +1,29 @@
 from i18n import i18n
-import time
 
-''' Function to generate the wikitext for the report page
-	@param wiki - Language code for the wiki (eg: 'en', 'es', 'it')
-	@param content - Python 2D array (nested list) containing table entries
-	@param desc - Key to the description message of the report
-'''
+""" Function to generate the wikitext for the report page
+    @param wiki - Language code for the wiki (eg: 'en', 'es', 'it')
+    @param content - Python 2D array (nested list) containing table entries
+    @param desc - Key to the description message of the report
+"""
 
-def display_report( wiki, content, desc ):
 
-	# Find out the dictionary containing messages for given wiki/language code
-	dict_obj = i18n.lang_dicts[ str( wiki + 'dict') ]
+def display_report(wiki, content, desc):
 
-	wikitext = dict_obj[ str( desc ) ] + ' -- ~~~ <onlyinclude>~~~~~</onlyinclude>\n'
-	wikitext = wikitext + '{| class="wikitable sortable" \n |- \n'
-	collen = len( content[0] )
-	rowlen = len( content )
+    # Find out the dictionary containing messages for given wiki/language code
+    dict_obj = i18n.lang_dicts[str(wiki + "dict")]
 
-	for x in range( 0, collen ):
-		wikitext = wikitext + '! ' + dict_obj[ str( content[0][x] ) ] + '\n'
+    wikitext = dict_obj[str(desc)] + " -- ~~~ <onlyinclude>~~~~~</onlyinclude>\n"
+    wikitext = wikitext + '{| class="wikitable sortable" \n |- \n'
+    collen = len(content[0])
+    rowlen = len(content)
 
-	for x in range( 1, rowlen ):
-		wikitext = wikitext + '|- \n'
-		for i in range( 0, collen ):
-			wikitext = wikitext + '| ' + str( content[x][i] ) + '\n'
+    for x in range(0, collen):
+        wikitext = wikitext + "! " + dict_obj[str(content[0][x])] + "\n"
 
-	wikitext = wikitext + '|}'
-	return wikitext
+    for x in range(1, rowlen):
+        wikitext = wikitext + "|- \n"
+        for i in range(0, collen):
+            wikitext = wikitext + "| " + str(content[x][i]) + "\n"
+
+    wikitext = wikitext + "|}"
+    return wikitext
--- test.py
+++ test.py
@@ -1,37 +1,58 @@
+import datetime
+
 import mwclient
 import pymysql
-import datetime
-from .config import *
-from reports import *
-import sys
-from displayTable import *
+
+from displayTable import display_report
+
+from .config import credentials, cttbot
+
 
 def forgotten_articles():
 
-	db = pymysql.connect( host = 'enwiki.labsdb', user = credentials['user'], passwd = credentials['pass'], db = 'enwiki_p' )
-	site = mwclient.Site( 'en.wikipedia.org' )
-	site.login( cttbot['user'], cttbot['pass'] )
-	print('initiated')
+    db = pymysql.connect(
+        host="enwiki.labsdb",
+        user=credentials["user"],
+        passwd=credentials["pass"],
+        db="enwiki_p",
+    )
+    site = mwclient.Site("en.wikipedia.org")
+    site.login(cttbot["user"], cttbot["pass"])
+    print("initiated")
 
-	# Make the query
-	cur = db.cursor()
-	query = """SELECT p.page_title, p.page_namespace, p.page_is_redirect, p.page_touched, r.editcount FROM page p
-			   LEFT JOIN ( SELECT COUNT(*) AS editcount, rev_page FROM revision GROUP BY rev_page ) r ON r.rev_page = p.page_id
-			   WHERE page_is_redirect = 0 AND page_namespace = 0
-			   ORDER BY page_touched
-			   LIMIT 500"""
-	cur.execute( query )
+    # Make the query
+    cur = db.cursor()
+    query = """SELECT p.page_title, p.page_namespace, p.page_is_redirect, p.page_touched, r.editcount FROM page p
+               LEFT JOIN ( SELECT COUNT(*) AS editcount, rev_page FROM revision GROUP BY rev_page ) r ON r.rev_page = p.page_id
+               WHERE page_is_redirect = 0 AND page_namespace = 0
+               ORDER BY page_touched
+               LIMIT 500"""
+    cur.execute(query)
 
-	# Extract the data into a Python nested list
-	content = []
-	content.append( ['forgotten-articles-title', 'forgotten-articles-last-edited', 'forgotten-articles-editcount'] )
-	for row in cur.fetchall() :
-		content.append( [ '[[' + row[0] + ']]', datetime.datetime.strptime( row[3],'%Y%m%d%H%M%S'), row[4] ] )
+    # Extract the data into a Python nested list
+    content = []
+    content.append(
+        [
+            "forgotten-articles-title",
+            "forgotten-articles-last-edited",
+            "forgotten-articles-editcount",
+        ]
+    )
+    for row in cur.fetchall():
+        content.append(
+            [
+                "[[" + row[0] + "]]",
+                datetime.datetime.strptime(row[3], "%Y%m%d%H%M%S"),
+                row[4],
+            ]
+        )
 
-	# Format the data as wikitext
-	text = display_report( 'en', content, 'forgotten-articles-desc' )
+    # Format the data as wikitext
+    text = display_report("en", content, "forgotten-articles-desc")
+    # May as well do something with this variable..
+    print(text)
+    page = site.Pages["Wikipedia:Database reports/Forgotten_articles"]
+    page.save(content, summary="bot test edit")
 
-	page = site.Pages[ 'Wikipedia:Database reports/Forgotten_articles']
-	page.save( content, summary = 'bot test edit' )
 
 forgotten_articles()
